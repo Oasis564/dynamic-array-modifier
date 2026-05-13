a96e34eda5ad0749bb0b32eeb5aeb838284ad7eb
usaid <usaid@OSAEIDCOMPUTER>
2026-05-13 20:19:58 +0100

    Add project files.

---
+++ dynamic array modifier/dynamic array modifier.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+using namespace std;
+const int max_size = 100;
+
+void listops() {
+    int myArray[max_size] = { 12,48,21,45,34};
+    
+    // initial size of the array.
+    int size = 5;
+
+    // print the origin array
+    for (int i = 0; i < size; i++) {
+        cout << myArray[i] << " " << endl;
+    }
+    cout << endl;
+
+    // insertion operation
+    int position, new_value;
+
+    // get the position at which to insert the new value
+    cout << "Enter the postion to enter a new value(0:Base Index): ";
+    cin >> position;
+    if (position < 0  position > size) {
+        cout
+    }
+}
+
+
+int main()
+{
+    //int myArray[10] = { 14,23,36,48,59,60,71,83,95,10 };
+
+    //// calculate the size of an array
+    ////int size = sizeof(myArray)/sizeof(myArray[0]);
+    ////cout << size;
+
+    //// print all the elements of an array
+    //for (int num : myArray) {
+    //    cout << num << endl;
+    //}
+
+    
+
+    
+}
+
